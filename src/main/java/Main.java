5f0072aaf96f5f813bae069c6423d954714ef597
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-16 14:28:50 +0900

    feat : XML parsing test 진행 중

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import controller.InitAPI;
+import model.ApiResponseModel;
 
 import java.io.IOException;
 
@@ -8,12 +9,29 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException {
-        InitAPI.init();
+        //InitAPI.init();
 
-        //testXML();
+        testXML();
     }
 
     public static void testXML() throws IOException {
+
+        String a = "<response>" +
+                    "<header>" +
+                        "<resultCode>00</resultCode>\n" +
+                        "<resultMsg>NORMAL SERVICE.</resultMsg>"+
+                    "</header>" +
+                    "</response>";
+        String b = "<response>" +
+                "<resultCode>00</resultCode>\n" +
+                "<resultMsg>NORMAL SERVICE.</resultMsg>"+
+                "</response>";
+
+        XmlMapper mapper = new XmlMapper();
+        mapper.setDefaultUseWrapper(false);
+        ApiResponseModel apiResponseModel = mapper.readValue( a, ApiResponseModel.class);
+        System.out.println(apiResponseModel.toString());
+
     }
 
 }

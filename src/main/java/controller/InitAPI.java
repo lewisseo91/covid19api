79a95a8a373af799e265ea421423c92538e86c5b
Eli Nabro <eli.nabro@kakaocorp.com>
2021-01-16 10:43:23 +0900

    feat : initAPI class 생성

---
+++ src/main/java/controller/InitAPI.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.util.List;
+
+public class InitAPI {
+
+    public static List<String> CovidDB;
+
+    public static void init() {
+
+    }
+
+    public static List<String> getCovidDB() {
+        return CovidDB;
+    }
+}
